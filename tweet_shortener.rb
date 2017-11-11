59a9b1233bd1f100167e04af481aa078067badac
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-11 14:10:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,45 @@
-# Write your code here.
+def dictionary(word)
+  convert = { "hello" => "hi",
+              "to" => "2",
+              "two" => "2",
+              "too" => "2",
+              "for" => "4",
+              "four" => "4",
+              "be" => "b",
+              "you" => "u",
+              "at" => "@",
+              "and" => "&"
+            }
+
+  final_word = word
+  #if word matches, return replacement, otherwise, return original word
+
+  if convert.key?(word)
+    final_word = convert[word]
+  elsif convert.key?(word.downcase)
+    final_word = convert[word.downcase].capitalize
+  end
+
+  final_word
+end
+
+def word_substituter(tweet)
+  words = tweet.split(' ')
+
+  new_tweet = words.collect do |word|
+    word = dictionary(word)
+  end
+
+  new_tweet.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.count > 140
+  
+end
